56b6ab04403ea51214733035601006e26cfbf7cd
JacquelineCristina <jacqueline.jcsilva@gmail.com>
2022-08-03 22:41:28 -0300

    ajuste telas"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarGrupoComponent } from './cadastrar-grupo/cadastrar-grupo.component';
+import { CadastrarPessoaComponent } from './cadastrar-pessoa/cadastrar-pessoa.component';
 import { DespesaComponent } from './despesa/despesa.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'dash', component: DespesaComponent },
+  { path: 'grupo', component: CadastrarGrupoComponent },
+  { path: 'pessoa', component: CadastrarPessoaComponent },
   { path: '', component: LoginComponent }
 ];
 
